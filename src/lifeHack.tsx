020365809f2450bb6319dd0b8d678f794ea041d9
ToshiyaKonno <tk.ichinoseki@gmail.com>
2020-12-08 21:37:55 +0900

    ピッカー完成

--- src/lifeHack.tsx
+++ src/lifeHack.tsx
@@ -3,24 +3,28 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, PickerIOSItem, Button,Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import RNPickerSelect from "react-native-picker-select";
-import tuna from "/Users/konnotoshiya/camp/01_react/OWDO/assets/images/tuna.jpg"; 
-import petbottle from "/Users/konnotoshiya/camp/01_react/OWDO/assets/images/petbottole.jpg"; 
+// import tuna from "../assets/images/tuna.jpg"; 
+// import petbottle from "../assets/images/petbottole.jpg"; 
 import React, { Component, useState } from "react";
 import { Value } from "react-native-reanimated";
 import { Item } from "react-native-paper/lib/typescript/src/components/Drawer/Drawer";
 
 
 let LF = "test";
-const imgArray = [petbottle,tuna]
-let imgNum = 1;
-imgNum = imgArray.length
-
+// const imgArray = [petbottle,tuna]
+const imgArray = [
+  require("../assets/images/petbottole.jpg"),
+  require("../assets/images/tuna.jpg"),
+];
+// let imgNum = 1;
+// imgNum = imgArray.length
 // let [count, setCount] = useState(100);
 
 
 //functionの名前をファイル名とおなじになるように変更
 //<Text>の表示する文字に画面の名前を設定
 export default function lifeHack() {
+  const [imgNum, setImgNum] = useState(0);
   return (
     <View style={styles.container}>
       <Text>{LF}</Text>
@@ -37,12 +41,11 @@ export default function lifeHack() {
           setCount((current) => current - 1);
         } */}
       <RNPickerSelect
-        onValueChange={(value) => console.log(value)}
+        onValueChange={(value: number) => setImgNum(value)}
         items={[
-          { label: "ペットボトルで簡易ランタン", value: petbottle },
-          { label: "ツナ缶でランプ", value: tuna },
+          { label: "ペットボトルで簡易ランタン", value: 0 },
+          { label: "ツナ缶でランプ", value: 1 },
         ]}
-        
       />
     </View>
   );
